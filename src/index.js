ed920e786dfa75f1c4f30b9713351c60be88180c
abhisheckpo <147988062+abhisheckpo@users.noreply.github.com>
2023-12-04 15:40:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import FontSizeAdjuster from './FontSizeAdjuster';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
-    <App/>
+    <FontSizeAdjuster/>
     </>
 );
 
