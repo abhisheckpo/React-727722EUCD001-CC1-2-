51a1bee0015939b14bc0f2367895a276cab1e975
abhisheckpo <147988062+abhisheckpo@users.noreply.github.com>
2023-12-04 15:48:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import './index.css';
-import App from './App';
+//import App from './App';
 import FontSizeAdjuster from './FontSizeAdjuster';
 //import reportWebVitals from './reportWebVitals';
 //import ColorButtons from './increment';
